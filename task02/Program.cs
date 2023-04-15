3e3dac17857ecda086d8548603c50bd8f982cf59
Elena <hardevich@gmail.com>
2023-04-15 22:28:50 +0400

    add hw01 task02

---
+++ task02/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите 1 числo: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2 числo: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 3 числo: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (max > numberB)
+  {
+    if (max > numberC)
+    {
+      System.Console.WriteLine($"MAX = {numberA}");
+    }
+    else
+    {
+      System.Console.WriteLine($"MAX = {numberC}");
+    }
+  }
+  
+else
+  {
+    if (numberB > numberC)
+    {
+      System.Console.WriteLine($"MAX = {numberB}");
+    }
+    else
+    {
+      System.Console.WriteLine($"MAX = {numberC}");
+    }
+  }
